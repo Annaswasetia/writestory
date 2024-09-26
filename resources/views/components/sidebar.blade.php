f97acd21d32a2b9adc973cfc5d00dc9685b12b30
Annaswa <anaswasetia@gmail.com>
2024-09-26 17:37:49 +0700

    components dan tampilan

---
+++ resources/views/components/sidebar.blade.php
@@ -0,0 +1,15 @@
+<a href="index.html" class="logo d-flex align-items-center">
+    <!-- Uncomment the line below if you also wish to use an image logo -->
+    <!-- <img src="assets/img/logo.png" alt=""> -->
+    <h1 class="sitename">Active.</h1>
+  </a>
+
+  <nav id="navmenu" class="navmenu">
+    <ul>
+      <li><a href="index.html" class="active">Home</a></li>
+      <li><a href="about.html">About</a></li>
+      <li><a href="services.html">Karya</a></li>
+      <li><a href="portfolio.html">Categori</a></li>
+    </ul>
+    <i class="mobile-nav-toggle d-xl-none bi bi-list"></i>
+  </nav>
