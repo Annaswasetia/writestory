18f45021bead6aeccdc6f47589be90c7a895ee62
Annaswa <anaswasetia@gmail.com>
2024-09-30 10:10:04 +0700

    memperbaiki tampilan awal

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -1,15 +1,22 @@
 <a href="index.html" class="logo d-flex align-items-center">
     <!-- Uncomment the line below if you also wish to use an image logo -->
     <!-- <img src="assets/img/logo.png" alt=""> -->
-    <h1 class="sitename">Active.</h1>
+    <h1 class="sitename">WriteaStory</h1>
   </a>
 
   <nav id="navmenu" class="navmenu">
     <ul>
       <li><a href="index.html" class="active">Home</a></li>
       <li><a href="about.html">About</a></li>
-      <li><a href="services.html">Karya</a></li>
-      <li><a href="portfolio.html">Categori</a></li>
+      <li class="nav-item">
+        <a class="nav-link" href="{{ route('karya.index') }}">Karya</a>
+    </li>
+    <li class="dropdown"><a href="#"><span>Category</span> <i class="bi bi-chevron-down toggle-dropdown"></i></a>
+      <ul>
+        <li><a href="#">Cerpen</a></li>
+        <li><a href="#">Puisi</a></li>
+      </ul>
+    </li>
     </ul>
     <i class="mobile-nav-toggle d-xl-none bi bi-list"></i>
   </nav>
--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,15 @@
 <?php
 
+use App\Http\Controllers\KaryaController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('layouts.app');
 });
 
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+route::resource('/karya', App\Http\Controllers\KaryaController::class);
